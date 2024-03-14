7dcadda06fe2f7bbd31c2817bc780f8ff8a23178
UrielBm <urielbenitez96@hotmail.com>
2024-03-14 17:07:46 -0600

    hotfix base loading and handlePost

--- src/pages/orthography/OrthographyPage.tsx
+++ src/pages/orthography/OrthographyPage.tsx
@@ -1,21 +1,54 @@
+import { useState } from "react";
 import GptMessage from "../../components/chat-bubbles/GptMessage";
 import TextMessageBox from "../../components/chat-input-boxes/TextMessageBox";
 import TypingLoader from "../../components/loaders/TypingLoader";
 import MyMessage from "../../components/myMessage/MyMessage";
 
+interface Messages {
+  message: string;
+  isGpt: boolean;
+}
+
 const OrthographyPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [mesagges, setMesagges] = useState<Messages[]>([]);
+
+  const handlePostMessages = async (message: string) => {
+    try {
+      setIsLoading(true);
+      setMesagges((prev) => [...prev, { message: message, isGpt: false }]);
+
+      //TODO: Usecase
+      setIsLoading(false);
+
+      //TODO: añadir el mensaje de backend de gpt
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="chat-container">
       <div className="chat-messages">
         <div className="grid grid-cols-12 gap-y-2">
           <GptMessage text="Hola soy el Bot de este chat, un gusto ayudarte con tu ortografía." />
-          <MyMessage text="Hola" />
-          <TypingLoader />
+          {mesagges.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text="mensaje que viene de back" />
+            ) : (
+              <MyMessage key={index} text={message.message} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12">
+              <TypingLoader />
+            </div>
+          )}
         </div>
       </div>
       <TextMessageBox
-        onSendMessage={(message) => console.log(message)}
-        placeholder="lo que sea"
+        onSendMessage={(message) => handlePostMessages(message)}
+        placeholder="escribe aquí tu mensaje"
         disabledCorrections={true}
       />
     </div>
