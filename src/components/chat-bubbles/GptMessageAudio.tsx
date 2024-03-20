3657a4e663841535a1fd18de4ef3b0b86d33ca6b
UrielBm <urielbenitez96@hotmail.com>
2024-03-19 22:57:59 -0600

    new feature: create a component audio

---
+++ src/components/chat-bubbles/GptMessageAudio.tsx
@@ -0,0 +1,21 @@
+interface Myprops {
+  text: string;
+  urlAudio: string;
+}
+
+const GptMessageAudio = ({ text, urlAudio }: Myprops) => {
+  return (
+    <div className="col-start-1 col-end-9 p-3 rounded-lg">
+      <div className="flex flex-row items-start">
+        <div className="flex items-center justify-center h-10 w-10 rounded-full bg-green-600 flex-shirink-0">
+          <i className="fa fa-cubes" aria-hidden="true"></i>
+        </div>
+        <div className="relative ml-3 text-sm bg-black bg-opacity-25 pt-3 pb-2 px-4 shadow rounded-x-xl">
+          <p>el mensaje convertido en audio es: {text}</p>
+          <audio controls src={urlAudio} autoPlay></audio>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default GptMessageAudio;
